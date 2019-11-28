b52dee1ab348a606b47d6287096b1a5fd4173039
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-11-28 20:25:44 +0100

    first commit

---
+++ build/precache-manifest.b643c758e2b3c1ae98cbe8a7b62983b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a69ea8ddd6d18f99391f626a1cced8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268cc538abd2090eed06",
+    "url": "/static/css/main.ac40300b.chunk.css"
+  },
+  {
+    "revision": "6ab8643790f8d4b941ff",
+    "url": "/static/js/2.4dd9ff6f.chunk.js"
+  },
+  {
+    "revision": "268cc538abd2090eed06",
+    "url": "/static/js/main.c4ee5fb4.chunk.js"
+  },
+  {
+    "revision": "389cab34a393392dace3",
+    "url": "/static/js/runtime-main.0c9b3bb9.js"
+  }
+]);
