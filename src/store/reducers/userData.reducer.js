e7272ba7222f8429244b0384b7a923f186ffadb2
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2020-01-05 12:05:10 +0100

    auth

--- src/store/reducers/userData.reducer.js
+++ src/store/reducers/userData.reducer.js
@@ -5,8 +5,9 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-
+    console.log(action.payload)
     switch (action.type) {
+
         case types.FETCH_USER_DATA:
         
             return {
